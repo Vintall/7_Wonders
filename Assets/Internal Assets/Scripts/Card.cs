51e9ba0f168b98e9863c08207a90603734f2bc44
Vintall <eg.ilyashenko@gmail.com>
2021-09-20 11:47:51 +0300

    Back sprite linker to [serializeField]

--- Assets/Internal Assets/Scripts/Card.cs
+++ Assets/Internal Assets/Scripts/Card.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
     [SerializeField] ScriptableCard card_data;
-    GameObject front_sprite;
-    GameObject back_sprite;
+    [SerializeField] GameObject front_sprite;
+    [SerializeField] GameObject back_sprite;
 
     private void Awake()
     {
-        front_sprite = transform.GetChild(0).gameObject;
-        back_sprite = transform.GetChild(1).gameObject;
+
     }
 
     [ExecuteInEditMode]
